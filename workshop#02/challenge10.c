c5a09750f67fafb37437d7269429e119a5a5d7b5
unknown <bousilmustapha@gmail.com>
2022-09-12 13:25:07 +0100

    succes challenge 10

--- workshop#02/challenge10.c
+++ workshop#02/challenge10.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int divededby(int *n,int *a,int *d){
+int divededby(int n,int a){
+  int d;
   bool t=true, f=false;
   int s=0;
-  *d=*n/(*a);
-  return *d;
+  d=n/a;
+  return d;
 
 
 }
-void divdebey(int *n , int *a){
-  bool t=true, f=false;
-
-  int d=divededby(&n,&a,&d);
-
+void divdebey(int n , int a){
+  int d=divededby(n,a);
+  //printf("%d",d);
   if(d==1 || d==2){
-    printf("True");
+    printf("Truezz");
   }else{
     for(int i=1;i<=d;i++){
     if(i!=1  && i!=d){
@@ -36,7 +35,7 @@ int main(){
     bool x;
     scanf("%d",&n);
     scanf("%d",&a);
-    divdebey(&n,&a);
+    divdebey(n,a);
 
 
 
